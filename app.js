6dc812e2f40ae7073bd6db7033b17b0fbaf07638
campbell <campbellrehu@me.com>
2017-04-24 10:00:29 +1200

    Updated app file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express'),
 app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'build/index.html'));
+    res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 app.listen(port, function(){
