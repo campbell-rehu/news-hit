2d2a610e234c023e35d8639c4ba9dacd166dd02e
campbell <campbellrehu@me.com>
2017-04-24 10:19:21 +1200

    Updated App

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express'),   
     app = express(),
-    port = process.env.PORT || 9000,
+    port = process.env.PORT || 7000,
     path = require('path');
 
 app.use(express.static(__dirname +'/build'));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/build/index.html'));
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, function(){
